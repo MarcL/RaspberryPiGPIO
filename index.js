bfbb607317c5a4c06990ddc275672e31edb5b77a
Marc Littlemore <marc.littlemore@bbc.co.uk>
2015-05-11 23:51:13 +0100

    Added registered task list.

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 var TaskRunner = require('./lib/TaskRunner');
-var SoundTask = require('./lib/tasks/SoundTask');
-var SleepTask = require('./lib/tasks/SleepTask');
+var registeredTasks = require('./lib/RegisteredTasks');
 
 var config = require('./assets/config');
 
-// Create task list
-// TODO: Create factory which creates instances of registered tasks
 var taskList = config.tasks.map(function(task) {
-    switch (task.type) {
-        case 'Sleep':
-            return new SleepTask(task.time);
-        case 'Sound':
-            return new SoundTask(task.filename);
-    }
+    return registeredTasks[task.type](task);
 });
 
 var taskRunner = new TaskRunner(taskList);
