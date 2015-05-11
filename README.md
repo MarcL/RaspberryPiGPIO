54d8b53bcd3128a9e177b6f69f6373e31c50230f
Marc Littlemore <marc.littlemore@bbc.co.uk>
2015-05-11 22:05:32 +0100

    Initial task to trigger sound effect.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # RaspberryPiGPIO
 Node.js project to start playing with GPIOs on a Raspberry Pi to trigger sound effects and control additional hardware.
+
+# Attribution
+* [Cuckoo Clock Sound](https://www.freesound.org/people/Syna-Max/sounds/58813/) by [Syna-Max](https://www.freesound.org/people/Syna-Max/) under CC BY-NC 3.0
--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-var Gpio = require('onoff').Gpio;
-var led = new Gpio(13, 'out');
-var button = new Gpio(4, 'in', 'both');
+// var Gpio = require('onoff').Gpio;
+// var led = new Gpio(13, 'out');
+// var button = new Gpio(4, 'in', 'both');
 
-button.watch(function(error, value) {
-	led.writeSync(value);
+// button.watch(function(error, value) {
+// 	led.writeSync(value);
+// });
+
+var SoundTask = require('./lib/tasks/SoundTask');
+var soundTask = new SoundTask('assets/sound/cuckoo-clock.mp3');
+soundTask.play().then(function(data) {
+    console.log(data);
+}, function(error) {
+    console.log('Error: ' + error);
 });
