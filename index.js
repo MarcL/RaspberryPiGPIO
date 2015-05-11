bc2d0e8a705d0db2c2c1f74e05e0b86361747a8c
Marc Littlemore <marc.littlemore@bbc.co.uk>
2015-05-11 23:36:43 +0100

    Added basic config file to setup task list

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ var TaskRunner = require('./lib/TaskRunner');
 var SoundTask = require('./lib/tasks/SoundTask');
 var SleepTask = require('./lib/tasks/SleepTask');
 
-var sleepTask = new SleepTask(3000);
-var soundTask = new SoundTask('assets/sound/cuckoo-clock.mp3');
+var config = require('./assets/config');
 
-var tasklist = [
-    sleepTask,
-    soundTask
-];
-var taskRunner = new TaskRunner(tasklist);
+// Create task list
+// TODO: Create factory which creates instances of registered tasks
+var taskList = config.tasks.map(function(task) {
+    switch (task.type) {
+        case 'Sleep':
+            return new SleepTask(task.time);
+        case 'Sound':
+            return new SoundTask(task.filename);
+    }
+});
+
+var taskRunner = new TaskRunner(taskList);
 taskRunner.execute();
