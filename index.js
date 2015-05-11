3513995cbca8417392a0be2d2503a01a96635f96
Marc Littlemore <marc.littlemore@bbc.co.uk>
2015-05-11 22:40:25 +0100

    Added TaskRunner to sequence tasks together.

--- index.js
+++ index.js
@@ -6,17 +6,25 @@
 // 	led.writeSync(value);
 // });
 
+var TaskRunner = require('./lib/TaskRunner');
 var SoundTask = require('./lib/tasks/SoundTask');
 var SleepTask = require('./lib/tasks/SleepTask');
 
 var sleepTask = new SleepTask(3000);
 var soundTask = new SoundTask('assets/sound/cuckoo-clock.mp3');
 
-sleepTask.execute().then(function(data) {
-    console.log(data);
-    return soundTask.execute();
-}).then(function(data) {
-    console.log(data);
-}, function(error) {
-    console.log('Error: ' + error);
-});
+var tasklist = [
+    sleepTask,
+    soundTask
+];
+var taskRunner = new TaskRunner(tasklist);
+taskRunner.execute();
+
+// sleepTask.execute().then(function(data) {
+//     console.log(data);
+//     return soundTask.execute();
+// }).then(function(data) {
+//     console.log(data);
+// }, function(error) {
+//     console.log('Error: ' + error);
+// });
