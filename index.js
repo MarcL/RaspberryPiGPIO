7b50d944f78abfedb6800bbd752f2bc8446594a5
Marc Littlemore <marc.littlemore@gmail.com>
2015-05-25 22:14:24 +0100

    Removed test LED code.

--- index.js
+++ index.js
@@ -1,24 +1,5 @@
-//var PiController = require('./lib/PiController');
-//var config = require('./assets/config');
+var PiController = require('./lib/PiController');
+var config = require('./assets/config');
 
-//var piController = new PiController(config);
-//piController.execute();
-
-var Gpio = require('onoff').Gpio;
-var led = new Gpio(2, 'out');
-
-var state = 0;
-
-console.log('Starting...');
-setInterval(changeLed, 400);
-
-function changeLed() {
-    led.writeSync(state);
-    state = (state === 1) ? 0 : 1;
-}
-
-process.on('SIGINT', function() {
-    console.log('Shutdown...');
-    led.unexport();
-    process.exit();
-});
+var piController = new PiController(config);
+piController.execute();
