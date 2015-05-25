2a9b14f579c5a1d1f845996431c77591d525c4e0
Marc Littlemore <marc.littlemore@gmail.com>
2015-05-25 21:55:30 +0100

    Reusing onoff and testing initial GPIO code.

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-var PiController = require('./lib/PiController');
-var config = require('./assets/config');
+//var PiController = require('./lib/PiController');
+//var config = require('./assets/config');
 
-var piController = new PiController(config);
-piController.execute();
+//var piController = new PiController(config);
+//piController.execute();
+
+var Gpio = require('onoff').Gpio;
+var led = new Gpio(2, 'out');
+
+var state = 0;
+
+setInterval(changeLed, 1000);
+
+function changeLed() {
+    console.log('LED: ' + state);
+    led.writeSync(state);
+    state = (state === 1) ? 0 : 1;
+}
