8cdc1dd9cc011abe771adb2295aa8780c08b8642
Marc Littlemore <marc.littlemore@gmail.com>
2015-05-31 11:39:02 +0100

    Added another test for TaskFactory.

--- test/TaskFactory.test.js
+++ test/TaskFactory.test.js
@@ -1,6 +1,19 @@
 var TaskFactory = require('../lib/TaskFactory');
+var RegisteredTasks = require('../lib/RegisteredTasks');
+var DelayTask = require('../lib/tasks/DelayTask');
+var LedTask = require('../lib/tasks/LedTask');
 
 describe('TaskFactory', function() {
+    beforeEach(function(done) {
+        RegisteredTasks.add('Delay', function(task) { return new DelayTask(task.time); });
+        RegisteredTasks.add('Led', function(task) { return new LedTask(task.gpio, task.time); });
+        done();
+    });
+
+    afterEach(function(done) {
+        RegisteredTasks.clear();
+        done();
+    });
     it('should throw exception if no tasks passed', function() {
         var fn = function() { new TaskFactory(); };
         expect(fn).to.throw(Error);
@@ -11,7 +24,7 @@ describe('TaskFactory', function() {
         expect(fn).to.throw('No tasks found');
     });
 
-    it('should throw exception if task in config is not registered', function() {
+    it('should throw exception if task is not registered', function() {
         var tasks = [
             {
                 type: 'UnknownTask'
@@ -19,5 +32,21 @@ describe('TaskFactory', function() {
 		];
         var fn = function() { new TaskFactory(tasks); };
         expect(fn).to.throw('Task not registered : UnknownTask');
+    });
+
+    it('should return task list containing correct number of registered tasks', function() {
+        var tasks = [
+            {
+                type: 'Delay',
+                time: 100
+            },
+            {
+                type: 'Led',
+                gpio: 2,
+                time: 200
+            }
+        ];
+        var taskList = new TaskFactory(tasks);
+        expect(taskList.length).to.equal(2);
     });
 });
