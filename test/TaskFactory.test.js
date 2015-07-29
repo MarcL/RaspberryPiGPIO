ef39eae212f4874293ff5b082be1e45fac8edb79
Marc Littlemore <marc.littlemore@gmail.com>
2015-07-29 23:27:30 +0100

    Fixed lint warnings.

--- test/TaskFactory.test.js
+++ test/TaskFactory.test.js
@@ -1,52 +1,111 @@
 var TaskFactory = require('../lib/TaskFactory');
 var RegisteredTasks = require('../lib/RegisteredTasks');
-var DelayTask = require('../lib/tasks/DelayTask');
-var LedTask = require('../lib/tasks/LedTask');
 
 describe('TaskFactory', function() {
-    beforeEach(function(done) {
-        RegisteredTasks.add('Delay', function(task) { return new DelayTask(task.time); });
-        RegisteredTasks.add('Led', function(task) { return new LedTask(task.gpio, task.time); });
-        done();
+    var registeredTasksCreateStub;
+    beforeEach(function() {
+        registeredTasksCreateStub = sinon.stub(RegisteredTasks, 'create');
     });
 
-    afterEach(function(done) {
-        RegisteredTasks.clear();
-        done();
+    afterEach(function() {
+        RegisteredTasks.create.restore();
     });
+
     it('should throw exception if no tasks passed', function() {
-        var fn = function() { new TaskFactory(); };
+        var fn = function() {
+            new TaskFactory();
+        };
         expect(fn).to.throw(Error);
     });
 
     it('should throw correct exception if no tasks passed', function() {
-        var fn = function() { new TaskFactory(); };
-        expect(fn).to.throw('No tasks found');
+        var fn = function() {
+            new TaskFactory();
+        };
+        expect(fn).to.throw('Task list must be defined.');
     });
 
-    it('should throw exception if task is not registered', function() {
-        var tasks = [
-            {
-                type: 'UnknownTask'
-            }
-		];
-        var fn = function() { new TaskFactory(tasks); };
-        expect(fn).to.throw('Task not registered : UnknownTask');
+    it('should throw exception if task list is not an array', function() {
+        var fn = function() {
+            new TaskFactory('');
+        };
+        expect(fn).to.throw(Error);
+    });
+
+    it('should throw correct exception if task list is not an array', function() {
+        var fn = function() {
+            new TaskFactory('');
+        };
+        expect(fn).to.throw('Task list must be an array of tasks.');
+    });
+
+    it('should throw exception if task list is empty', function() {
+        var fn = function() {
+            new TaskFactory([]);
+        };
+        expect(fn).to.throw(Error);
+    });
+
+    it('should throw correct exception if task list is empty', function() {
+        var fn = function() {
+            new TaskFactory([]);
+        };
+        expect(fn).to.throw('Task list must not be empty.');
+    });
+
+    it('should call registeredTasks create', function() {
+        var defaultTasks = [
+            { type: 'KnownTask' }
+        ];
+        new TaskFactory(defaultTasks);
+        expect(registeredTasksCreateStub.called).to.be.equal(true);
+    });
+
+    it('should call registeredTasks create with expected task', function() {
+        var defaultTasks = [
+            {type: 'expected type'}
+        ];
+        new TaskFactory(defaultTasks);
+        expect(registeredTasksCreateStub.getCall(0).args[0]).to.be.equal(defaultTasks[0]);
+    });
+
+    it('should call registeredTasks create expected number of times', function() {
+        var defaultTasks = [
+            {type: 'expected type'},
+            {type: 'expected type'},
+            {type: 'expected type'}
+        ];
+        new TaskFactory(defaultTasks);
+        expect(registeredTasksCreateStub.callCount).to.be.equal(defaultTasks.length);
+    });
+
+    it('should call registeredTasks create with expected tasks', function() {
+        var defaultTasks = [
+            {type: 'expected type'},
+            {type: 'expected type 2'},
+            {type: 'expected type 3'}
+        ];
+        new TaskFactory(defaultTasks);
+        for (var i = 0; i < defaultTasks.length; i++) {
+            expect(registeredTasksCreateStub.getCall(i).args[0]).to.be.equal(defaultTasks[i]);
+        }
     });
 
     it('should return task list containing correct number of registered tasks', function() {
+        var MockTask = function() {};
+
+        RegisteredTasks.add('Task1', function() { return new MockTask(); });
+        RegisteredTasks.add('Task2', function() { return new MockTask(); });
         var tasks = [
             {
-                type: 'Delay',
-                time: 100
+                type: 'Task1'
             },
             {
-                type: 'Led',
-                gpio: 2,
-                time: 200
+                type: 'Task2'
             }
         ];
         var taskList = new TaskFactory(tasks);
-        expect(taskList.length).to.equal(2);
+        expect(taskList.length).to.equal(tasks.length);
+        RegisteredTasks.clear();
     });
 });
